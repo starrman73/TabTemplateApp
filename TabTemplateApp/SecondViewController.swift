3409d91d29f774e423b5f99e4bb20b548ccbe54d
Steven Eldridge <stevenstarr@stevens-mbp.home>
2017-09-16 18:57:37 -0400

    Initial Commit

---
+++ TabTemplateApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TabTemplateApp
+//
+//  Created by Steven Starr Eldridge on 9/16/17.
+//  Copyright © 2017 Steven Starr Eldridge. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
